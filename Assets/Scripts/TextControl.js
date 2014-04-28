3bbb49191f667ec20fffdf824a468bb6ee4233f6
Christina Lidwin <clidwin@gmail.com>
2014-04-27 21:05:29 -0400

    Menu Part 1
    Start of the Main Menu, including javascript, possible setup, and font
    importing

---
+++ Assets/Scripts/TextControl.js
@@ -0,0 +1,10 @@
+﻿//From Tutorial: https://www.youtube.com/watch?v=7UbhbxSDcTs
+
+function OnMouseEnter(){
+	//Change the color of the text
+	renderer.material.color = Color.green;
+}
+
+function OnMouseExit(){
+	renderer.material.color = Color.white;
+}
