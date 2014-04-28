095034b05f060ee1874998f6b22d98678093c3ca
Wil Collins <wil.collins@gmail.com>
2014-04-28 01:55:07 -0400

    new shadow texturing

--- Assets/Scripts/SamCam.js
+++ Assets/Scripts/SamCam.js
@@ -1,5 +1,10 @@
-﻿var subject : GameObject;
+﻿static final var SAM_NAME : String = "Sam";
 
+private var subject : Transform;
+
+function Start () {
+	subject = GameObject.Find(SAM_NAME).transform;
+}
 function LateUpdate () {
-	transform.position.x = subject.transform.position.x;
+	transform.position.x = subject.position.x;
 }
