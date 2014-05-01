66ef23ea95c5712b52511cac2077316c90093472
Anne Lee <annelee@vt.edu>
2014-05-01 17:01:32 -0400

    gui effect

--- Assets/Scripts/SamCam.js
+++ Assets/Scripts/SamCam.js
@@ -1,10 +1,15 @@
 ﻿static final var SAM_NAME : String = "Sam";
 
 private var subject : Transform;
+var texture : Texture;
 
 function Start () {
 	subject = GameObject.Find(SAM_NAME).transform;
 }
 function LateUpdate () {
 	transform.position.x = subject.position.x;
+}
+
+function OnGUI() {
+	GUI.DrawTexture(Rect(0, 0 , Screen.width, Screen.height), texture);
 }
