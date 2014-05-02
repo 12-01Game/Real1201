9ed480ed820f78513526c7e52aea096eb7a5c768
Roger <yamada13@vt.edu>
2014-05-02 15:36:09 -0400

    Patch Notes: -Level transitioning exists, currently no fade in/out on transition

--- Assets/Scripts/LevelManager.js
+++ Assets/Scripts/LevelManager.js
@@ -6,17 +6,25 @@ private final var samSuccess	: String = "standard_idle_1";
 private final var hankSuccess	: String = "walking";
 
 private var level 				: int;
-private var CameraFade 			: CameraFade;
+private var cameraFade 			: CameraFade;
 
 private var samAnimation		: Animation;
 private var hankAnimation		: Animation;
 
-private var scenes : String[] = ["BedRoom", "Living Room"];
+private var scenes : String[] = ["Level1", "Level2", "Level3"];
 
 function Start () {
-	level = -1;
+	level = 0;
 	samAnimation = GameObject.Find(SAM_NAME).GetComponentsInChildren(Animation)[0];
 	hankAnimation = GameObject.Find(HANK_NAME).GetComponentsInChildren(Animation)[0];
+	cameraFade = GetComponent(CameraFade);
+}
+
+function OnTriggerEnter(collider : Collider) {
+	if (collider.gameObject.tag == "Player") { //ensures it's Sam
+		LevelSuccess();
+		print("'Murica FUCK YEA!");
+	}
 }
 
 function LevelRestart(){
@@ -27,11 +35,12 @@ function LevelSuccess(){
 	samAnimation.Play(samSuccess);
 	hankAnimation.Play(hankSuccess);
 
-	yield WaitForSeconds (3);
+	yield WaitForSeconds (2);
 
 	LevelTransition();
 }
 
+/* Do not call this function directly, use LevelSuccess instead */
 function LevelTransition(){
 	CameraFadeOut();
 	NextLevel();
@@ -42,17 +51,17 @@ function LevelTransition(){
 function NextLevel(){
 	if(level == scenes.length - 1){
 		// FINAL CUTSCENE
+		print("FINAL??");
 	}else{
 		level++;
 		Application.LoadLevel(scenes[level]);
 	}
-
 }
 
 function CameraFadeIn(){
-	CameraFade.fadeOut();
+	cameraFade.fadeOut();
 }
 
 function CameraFadeOut(){
-	CameraFade.fadeIn();
+	cameraFade.fadeIn();
 }
