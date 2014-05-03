74c4ea67cee90b261a1c671a0227467f376c3ef1
saskaeto <p.starrin@gmail.com>
2014-05-03 16:11:50 -0400

    Update activateLightSwitch.js
    Tweaked this and shadowbarrier2.js to fix a bug where the game would start with all of the light switches already on.

--- Assets/Scripts/activateLightSwitch.js
+++ Assets/Scripts/activateLightSwitch.js
@@ -4,13 +4,18 @@ var light_sources = new GameObject[0];
 var player : Transform;
 var distanceX : float;
 var distanceY : float;
-var which_button : String;
-private static var DEFAULT_SHADER = Shader.Find("Diffuse");
-private static var HIGHLIGHTED_SHADER = Shader.Find("Self-Illumin/Outlined Diffuse");
+var on : boolean = false;
 
-function Start() {
+private var which_button : String;
+private var defaultShader;
+private var highlightedShader;
+
+function Awake() {
+    defaultShader = Shader.Find("Diffuse");
+    highlightedShader = Shader.Find("Self-Illumin/Outlined Diffuse");
+
 	for (var i = 0; i < light_sources.length; i++) {
-				light_sources[i].light.enabled = false;
+		light_sources[i].light.enabled = false;
 	}
 }
 
@@ -18,14 +23,16 @@ function Update() {
 	which_button = "HankAction";
 	distanceX = Mathf.Abs(this.transform.position.x - player.position.x);
 	distanceY = Mathf.Abs(this.transform.position.y - player.position.y);
-	if (distanceX < 1 && distanceY < 1){
-		renderer.material.shader = HIGHLIGHTED_SHADER;
+	if (!on && distanceX < 1 && distanceY < 1) {
+		renderer.material.shader = highlightedShader;
 		if (Input.GetButtonDown(which_button)) {
+            on = true;
 			for (var i = 0; i < light_sources.length; i++) {
 				light_sources[i].light.enabled = true;
+                light_sources[i].GetComponent(shadowbarrier2).TurnOn();
 			}
 		}
 	} else {
-		renderer.material.shader = DEFAULT_SHADER;
+		renderer.material.shader = defaultShader;
 	}
 }
